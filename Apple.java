42c32ab276574230918afd3d1e5b2bff140c4785
Levi4688 <350647954@gapps.yrdsb.ca>
2024-05-01 20:55:35 -0400

    1.1-3

---
+++ Apple.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Food for our elephant.
+ * 
+ * @author Levi 
+ * @version May 2024
+ */
+public class Apple extends Actor
+{
+    /**
+     * Act - do whatever the Apple wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        setLocation(getX(), getY() + 1);
+        
+    }
+}
